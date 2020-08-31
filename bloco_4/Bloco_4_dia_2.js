cca671fe6b14c76b27160ab5741d8ff7c0fb5bf8
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-08-31 13:34:19 -0300

    questao 5

--- bloco_4/Bloco_4_dia_2.js
+++ bloco_4/Bloco_4_dia_2.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let totalsum = 0;
+
 for (let indice = 0; indice< numbers.length; indice +=1){
     totalsum=totalsum + numbers[indice];
 }
@@ -9,4 +10,14 @@ console.log(`A média aritimética do array é ${arithmeticAverage} `);
 if (arithmeticAverage< 20){
     console.log(` valor maior que 20`)}
     else { console.log(`valor menor que 20`)
+    };
+
+let higherNumber = 0;
+
+for (let indice = 0; indice< numbers.length; indice +=1){
+    if (higherNumber < numbers[indice]) {
+            higherNumber = numbers[indice]
     }
+};
+
+console.log(`O maior numero desse array é o  ${higherNumber} `);
